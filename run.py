76e64a315ce3a14cabc9e8ca77cd83fa425281ed
plehdeen <130137243@qq.com>
2018-04-23 17:06:26 +0800

    Fix skb->head changed bug when skb_cow happen on ip_forward

---
+++ run.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import os
+import sys
+class record:
+	def __init__(self):
+		self.func_id=[]
+		self.tstamp=[]
+		self.proto=''
+		self.src_ip=''
+		self.dst_ip=''
+		self.sport=''
+		self.dport=''
+		self.data=''
+	def clear(self):
+		self.func_id=[]
+		self.tstamp=[]
+		self.proto=''
+		self.src_ip=''
+		self.dst_ip=''
+		self.sport=''
+		self.dport=''
+		self.data=''
+
+def output(key):
+	item=addrhash[key]
+	out_string=""
+	out_string+=item.src_ip+"/"
+	out_string+=item.dst_ip+"/"
+	out_string+=item.proto+"/"
+	out_string+=item.sport+"/"
+	out_string+=item.dport+"/"
+	out_string+=str(len(item.func_id))+"/"
+	for i in item.func_id:
+		out_string+=i+"/"
+	for i in item.tstamp:
+		out_string+=i+"/"
+	out_string=out_string[:-1]+"\n"
+	f.write(out_string)
+
+
+SKB=0
+SKB_HEAD=1
+DATA=2
+SADDR=3
+DADDR=4
+PROTO=5
+SPORT=6
+DPORT=7
+FUNC_ID=8
+TSTAMP=9
+
+filename = sys.argv[1]
+outfile = sys.argv[2]
+
+#s = os.popen('awk \'{for(i=6;i<=12;i++){print $i""FS} print ""}\'' + filename).read()
+s = open(filename,'r').read().split('\n')[:-1]
+
+func_table=[
+	('skb_free_head',				4),
+	('ip_rcv',						0),
+	('__netif_receive_skb_core',	0),
+	('ip_local_deliver',			3),
+	('ip_local_out',				0),
+	('ip_output',					0),
+	('__dev_queue_xmit',			0),
+	('napi_gro_receive',			1),
+	('udp_send_skb',				2),
+	('tcp_transmit_skb',			2),
+	('br_handle_frame_finish',		0),
+	('netif_receive_skb_internal',	0),
+	('ovs_vport_receive',			0),
+	('ovs_execute_actions',			0),
+	('e100_xmit_frame',				3),
+	('ixgbe_xmit_frame',			3),
+	('ip_rcv_finish',				0),
+	('ip_forward',					0),
+	('ip_forward_finish',			0),
+]
+
+addrhash=dict()
+
+f=open(outfile,'w')
+
+#count=0
+for i in s:
+#	count=count+1
+#	print count
+	temp= i.split(' ')
+
+	if temp[1]=='==':	#switch the key
+		if not addrhash.has_key(temp[2]):
+			addrhash[temp[2]]=record()
+		addrhash[temp[2]].clear()
+		for j in addrhash[temp[0]].func_id:
+			addrhash[temp[2]].func_id.append(j)
+		for j in addrhash[temp[0]].tstamp:
+			addrhash[temp[2]].tstamp.append(j)
+		addrhash[temp[2]].src_ip = addrhash[temp[0]].src_ip
+		addrhash[temp[2]].dst_ip = addrhash[temp[0]].dst_ip
+		addrhash[temp[2]].proto = addrhash[temp[0]].proto
+		addrhash[temp[2]].sport = addrhash[temp[0]].sport
+		addrhash[temp[2]].dport = addrhash[temp[0]].dport
+		addrhash[temp[2]].data = addrhash[temp[0]].data
+		addrhash[temp[0]].clear()
+		continue
+
+	skb = temp[SKB]
+	key = temp[SKB_HEAD]
+	data = temp[DATA]
+	func_id = temp[FUNC_ID]
+	tstamp = temp[TSTAMP]
+	src_ip = temp[SADDR]
+	dst_ip = temp[DADDR]
+	proto = temp[PROTO]
+	sport = temp[SPORT]
+	dport = temp[DPORT]
+
+	if addrhash.has_key(key):
+
+		if func_table[int(func_id)][1]==4 or func_table[int(func_id)][1]==3:		# it's the end function, del the key
+
+			# record into another value
+			#print addrhash[key].proto
+			addrhash[key].func_id.append(func_id)
+			addrhash[key].tstamp.append(tstamp)
+			if addrhash[key].proto!='':
+				if  int(addrhash[key].proto) ==6  or int(addrhash[key].proto) ==17:
+					output(key)
+			addrhash[key].clear()
+			continue
+		if addrhash[key].data!='' and data!=addrhash[key].data:
+			if func_table[int(func_id)][1]!=1 and func_table[int(func_id)][1]!=2:
+				last_id = addrhash[key].func_id.pop()
+				last_tstamp=addrhash[key].tstamp.pop()
+				if addrhash[key].proto!='':
+					if int(addrhash[key].proto) ==6  or int(addrhash[key].proto) ==17:
+						output(key)
+				addrhash[key].clear()
+				addrhash[key].func_id.append(last_id)
+				addrhash[key].tstamp.append(last_tstamp)
+
+		addrhash[key].func_id.append(func_id)
+		addrhash[key].tstamp.append(tstamp)
+		if func_table[int(func_id)][1]!=1 and func_table[int(func_id)][1]!=2: 		
+			addrhash[key].src_ip = src_ip
+			addrhash[key].dst_ip = dst_ip
+			addrhash[key].proto = proto
+			addrhash[key].sport = sport
+			addrhash[key].dport = dport
+			addrhash[key].data = data
+	else:
+		# insert a key
+		if func_table[int(func_id)][1]==1 or func_table[int(func_id)][1]==2 :
+			item=record()
+			addrhash[key]=item;
+			addrhash[key].func_id.append(func_id)
+			addrhash[key].tstamp.append(tstamp)
+
+f.close()
--- write_3.c
+++ write_3.c
@@ -27,13 +27,15 @@
 #include <asm/atomic.h>
 #include <asm/cmpxchg.h>
 
-#define CPU_NUM 1
-#define FUNC_TABLE_SIZE 16
+#define CPU_NUM 4
+#define FUNC_TABLE_SIZE 19
+#define SPEC_FUNC_TABLE_SIZE 1
 #define ADDR_HEAD_SIZE 100000
 #define HASH_INTERVAL 12345
 #define SAMPLE_RATIO 1000000000
 extern void my_pre_handler(void);
-//extern void my_ret_handler(void);
+extern void my_pre_handler_2(void);
+extern void my_ret_handler(void);
 
 
 struct func_table{
@@ -85,12 +87,18 @@ static struct func_table my_func_table[FUNC_TABLE_SIZE]={
 // two driver, e1000/e1000e  ixgbe
 	{0,0,0,0,3,"e1000_xmit_frame"},				// e1000_xmit_frame(struct sk_buff *skb, struct net_device *netdev);
 	{0,0,0,0,3,"ixgbe_xmit_frame"},				// ixgbe_xmit_frame(struct sk_buff *skb, struct net_device *netdev);
+	{0,0,2,0,0,"ip_rcv_finish"},				// ip_rcv_finish(struct net *net, struct sock *sk, struct sk_buff *skb);
+	{0,0,0,0,0,"ip_forward"},					// ip_forward(struct sk_buff *skb)
+	{0,0,2,0,0,"ip_forward_finish"},			// ip_forward_finish(struct net *net, struct sock *sk, struct sk_buff *skb)
 };
 
-//static struct func_delay_record my_delay_record[CPU_NUM][FUNC_TABLE_SIZE];
+// spec func table is for function which change the skb->head
+// and this kind of function need the ret
+static struct func_table my_spec_func_table[SPEC_FUNC_TABLE_SIZE]={
+	{0,0,0,0,0,"pskb_expand_head"},				// pskb_expand_head(struct sk_buff *skb, int nhead, int ntail, gfp_t gfp_mask)
+};
 
-//unsigned long addr;
-//u32 content;
+// addr 3 is for atomic_modifying_code
 atomic_t * addr3;
 const unsigned char brk = 0xcc;
 const unsigned char call= 0xe8;
@@ -148,82 +156,30 @@ static inline void output(struct sk_buff *skb, struct timespec *time, int funcid
 
 	struct iphdr *iph;
 	struct tcphdr *tcph;
+	u32 data;
 	u64 tstamp;
 	tstamp = (time->tv_sec << 32) | time->tv_nsec;
 
 	iph= ip_hdr(skb);
 	tcph= tcp_hdr(skb);
-	trace_printk("%p %x %x %d %d %d %d %llx\n", skb->head, iph->saddr, iph->daddr, iph->protocol, tcph->source , tcph->dest, funcid, tstamp);
+	data = *((u32*)(skb->head + skb->network_header + 4));
+
+
+	trace_printk("%p %p %x %x %x %d %d %d %d %llx\n",skb, skb->head, data, iph->saddr, iph->daddr, iph->protocol, tcph->source , tcph->dest, funcid, tstamp);
 //	return iph->protocol;
 }
-#if 0
-void testfunction_ret_1(void){
-
-	int cpu;	
-
-	//ret function
-	//trace_printk("hello ret\n");
-	unsigned long func;
-	struct timespec time;
-	struct timespec time2;
-	int i;
-	unsigned long status=0;
-	unsigned long time_used;
-
-	__asm__ __volatile__(
-		"movq 0xa0(%%rbp), %0;"		// get the status
-		: "=a"(status)
-		::
-	);
-
-	if(status==0)
-		goto out;
-
-	__asm__ __volatile__(
-		"movq  0xa8(%%rbp), %0;"		// get the function addr
-		"movq  0x98(%%rbp), %1;"		// get the start timestamp, second
-		"movq  0x90(%%rbp), %2;"		// nanosecond
-		: "=a"(func), "=b"(time.tv_sec), "=c"(time.tv_nsec)
-		:
-		:
-	);
-
-	for(i=0;i<FUNC_TABLE_SIZE;i++){
-		if(func == my_func_table[i].addr +5 ){
-			
-			// do something statistic here
-			getnstimeofday(&time2);
-			time_used = ((time2.tv_sec - time.tv_sec))*10000000 + (time2.tv_nsec - time.tv_nsec)/100;
-			
-			if(time_used >= FUNC_RECORD_SIZE)
-				time_used = FUNC_RECORD_SIZE-1;
-
-			cpu=smp_processor_id();
-			if(cpu >= CPU_NUM){
-				printk(KERN_ALERT "module write CPU_NUM is less than core numbers\n");
-				goto out;
-			}
-			//trace_printk("cpu now is %d delay is %d(100ns)\n", cpu, time_used);
-			my_delay_record[cpu][i].count[time_used]++;
-			
-			goto out;
-		}
-	}
-	//printk(KERN_INFO "before is %p.%p after is %p.%p\n", time.tv_sec, time.tv_nsec, time2.tv_sec, time2.tv_nsec);
-out:
-	;
-}
-#endif
-static inline void remove_hash(int cpu, int idx, struct sk_buff *skb){
+static inline void remove_hash(int cpu, int idx, unsigned long addr){
 	struct addr_list *prev, *temp;	
 
 Again: 
 	prev = &(addrHead[cpu][idx].lhead);
 	temp=prev->next;
-	while(temp->addr!=(unsigned long)(skb->head)){
+	while(temp!=NULL && temp->addr!= addr){
 		prev=temp;
 		temp=temp->next;
 	}
+	if(temp==NULL)
+		goto rm_out;	
 	local_irq_disable();
 	if(prev->next!=temp){
 		local_irq_enable();
@@ -232,15 +188,18 @@ Again:
 	prev->next=temp->next;
 	local_irq_enable();
 	kmem_cache_free(myslab, temp);
+
+rm_out:
+	;
 }
-static inline void insert_hash(int cpu, int idx, struct sk_buff *skb){
+static inline void insert_hash(int cpu, int idx, unsigned long addr){
 	struct addr_list *prev, *temp;	
 	temp = kmem_cache_alloc(myslab, GFP_KERNEL);
 	if(temp==NULL){
 		printk(KERN_ALERT "kmem_cache_alloc Failed\n");
 		goto i_out;
 	}
-	temp->addr= (unsigned long)skb->head;
+	temp->addr= addr;
 
 	local_irq_disable();
 	prev = &(addrHead[cpu][idx].lhead);
@@ -256,79 +215,111 @@ static inline int check_frame(struct sk_buff *skb){
 		return 0;
 	return 1;
 }
+void testfunction_ret_1(void){
+
+	int cpu;	
+
+	//ret function
+	//trace_printk("hello ret\n");
+	//unsigned long func;
+	//struct timespec time;
+	//struct timespec time2;
+	//int i;
+	unsigned long status;
+	//unsigned long time_used;
+	struct sk_buff *skb;
+	unsigned long head;
+	unsigned long base=1;
+	int idx;
+
+	__asm__ __volatile__(
+		"movq %%rdi, %0\n"
+		"movq 0x80(%0), %1\n"		// get the status
+		: "+a"(base), "=b"(status)
+		::
+	);
+	if(status==0)
+		goto out;
+
+	__asm__ __volatile__(
+		"movq  0x90(%2), %0;"		// get the skb
+		"movq  0x88(%2), %1;"		// get the origin skb->head
+		: "=b"(skb), "=d"(head)
+		: "a"(base)
+		:
+	);
+	//if( (unsigned long)(skb->head) == head)	// impossible
+	//	goto out;
+	cpu = smp_processor_id();
+	if(cpu >= CPU_NUM){
+		printk(KERN_ALERT "CPU_NUM is less than the cpu core numbers\n");
+		goto out;
+	}
+#if 1
+	if(search(head, &idx, cpu)== 0){
+		remove_hash(cpu, idx, head);
+		trace_printk(" %p == %p\n", (void*)head, skb->head);
+		search((unsigned long)(skb->head), &idx, cpu);
+		insert_hash(cpu, idx, (unsigned long)(skb->head));
+	}
+#else
+		trace_printk(" %p == %p\n", (void*)head, skb->head);
+#endif
+
+out:
+	;
+}
 void testfunction_1(void){
 // invisible instruction
 //	push $rbp
 //	mov $rsp, $rbp
 
-//	unsigned long function_ret;
 	unsigned long func;
 	struct timespec time;
 	struct sk_buff * skb;
-	int i;
 	unsigned long offset;
+	unsigned long base=1;
+	int i;
 	int idx;
 	int cpu;
-//	u16 protocol;
-	//struct addr_list *prev, *temp;
-	//add your own code here
 
-//	function_ret=(unsigned long)my_ret_handler;
-//	getnstimeofday(&time);
-//	cpu = smp_processor_id();	
-	
-//	trace_printk("cpu is %d\n", get_cpu());
-//	put_cpu();
 	__asm__ __volatile__(
-//		"movq %1, 0x68(%%rbp);"		// timestamp second
-//		"movq %2, 0x60(%%rbp);"		// timestamp nanosecond
-//		"movq %1, 0x58(%%rbp);"		// set the function ret addr
-		"movq 0x50(%%rbp), %0;"		// get the function addr
-		: "=a"(func)
-//		: "b"(time.tv_sec), "c"(time.tv_nsec),"d"(function_ret)
-//		: "d"(function_ret)
+		"movq %%rdi, %0\n"
+		"movq 0x40(%0), %1\n"		// get the function addr
+		: "+a"(base), "=b"(func)
 		:
 		:
 	);
 
 	getnstimeofday(&time);
 #if 1
-	//trace_printk("this func addr is %p\n", func);
 	for(i=0;i<FUNC_TABLE_SIZE;i++){
 		if(func == my_func_table[i].addr +5){
 			
-			offset = 0x10 + my_func_table[i].skb_idx *8;
+			offset = my_func_table[i].skb_idx *8;
 
 			__asm__ __volatile__(
-//			"movq $1, 0x70(%%rbp);"		// set the status to 1
-//			"movq %0, 0x68(%%rbp);"		// timestamp second
-//			"movq %1, 0x60(%%rbp);"		// timestamp nanosecond
-			"addq %%rbp, %1\n"
-			"movq (%%rax), %0\n"
-			: "=c"(skb)
-//			: "b"(time.tv_sec), "c"(time.tv_nsec)
-			: "a"(offset)
+		//	"addq %0, %1\n"
+			"movq (%1, %2), %0\n"
+			: "=d"(skb)
+			: "a"(base), "b"(offset)
 			:
 			);
 			cpu = smp_processor_id();
 			if(cpu >= CPU_NUM){
 				printk(KERN_ALERT "CPU NUM is less than the cpu core numbers\n");
-				goto out;
+				goto out1;
 			}
+#if 0
+			output(skb, &time, i);
+#else
 			if(search((unsigned long)(skb->head), &idx, cpu)==0){
 				// find 
-			//	if(my_func_table[i].flag == 2){
-			//		remove_hash(cpu, idx, skb);
-			//		if(time.tv_nsec < SAMPLE_RATIO){
-			//			output(skb, &time, i);
-			//			insert_hash(cpu, idx, skb);
-			//		}
-			//		goto out;
-			//	}
+
 				output(skb, &time, i);
 
 				if(my_func_table[i].flag == 3 || my_func_table[i].flag==4){
-					remove_hash(cpu, idx, skb);
+					remove_hash(cpu, idx, (unsigned long)(skb->head));
 				}
 	
 			}else{
@@ -337,27 +328,77 @@ void testfunction_1(void){
 					if(time.tv_nsec < SAMPLE_RATIO){
 						
 						if ( !check_frame(skb) )
-							goto out;
+							goto out1;
 						//output
 						output(skb, &time, i);
 
 						//insert into hash
-						insert_hash(cpu, idx, skb);
+						insert_hash(cpu, idx, (unsigned long)(skb->head));
 					}
 				}
 			}
+#endif
 
-			goto out;
+			goto out1;
 		}
 	}
-	//printk(KERN_INFO "start func is %p function ret is %p timestamp %p.%p\n", func, function_ret, time.tv_sec, time.tv_nsec); 
-//	__asm__ __volatile__(
-//	"movq $0, 0x70(%%rbp);"		// set the status to 0
-//	:::
-//	);	
-out: 
+out1: 
 	;
 #endif
+}
+void testfunction_2(void){
+// invisible instruction
+//	push $rbp
+//	mov $rsp, $rbp
+
+	unsigned long function_ret;
+	unsigned long func;
+	//struct timespec time;
+	struct sk_buff * skb;
+	unsigned long offset;
+	unsigned long base=1;
+	int i;
+	//int idx;
+
+	function_ret = (unsigned long)my_ret_handler;
+	__asm__ __volatile__(
+		"movq %%rdi, %0\n"
+		"movq %2, 0x48(%0)\n"		// change the return addr
+		"movq 0x40(%0), %1\n"		// get the function addr
+		: "+a"(base),"=b"(func)
+		: "d"(function_ret)
+		:
+	);
+	for(i=0;i<SPEC_FUNC_TABLE_SIZE;i++){
+		if(func == my_spec_func_table[i].addr + 5){
+			offset = my_spec_func_table[i].skb_idx *8;
+			__asm__ __volatile__(
+			//	"addq %%rbp, %1\n"
+				"movq (%1, %2), %0\n"
+				: "=d"(skb)
+				: "a"(base), "b"(offset)
+				:
+			);
+			__asm__ __volatile__(
+				"movq %1, 0x60(%0)\n"		// store the skb
+				"movq %2, 0x58(%0)\n"		// store the skb->head
+				"movq $1, 0x50(%0)\n"		// status
+				: 
+				: "a"(base), "d"(skb), "b"(skb->head)
+				:
+			);
+			goto out2;
+		}
+	}
+	// if not found one, then we should set status=0
+	__asm__ __volatile__(
+		"movq $0, 0x50(%0)\n"
+		:
+		: "a"(base)
+		:
+	);
+out2:
+	;
 }
 
 static void code_modify(struct func_table* func, unsigned long target_addr){
@@ -424,6 +465,7 @@ static int __init my_write_init(void)
 	int j;
 	
 	//init slab
+	//printk(KERN_INFO "sizeof skb is %d\n", sizeof(struct sk_buff));
 	myslab = kmem_cache_create("myslab", sizeof(struct addr_list), 0, 
 				SLAB_HWCACHE_ALIGN | SLAB_POISON | SLAB_RED_ZONE, NULL);
 
@@ -441,6 +483,10 @@ static int __init my_write_init(void)
 		my_func_table[i].addr = kallsyms_lookup_name(my_func_table[i].name);
 		code_modify( &(my_func_table[i]), (unsigned long)my_pre_handler);
 	}
+	for(i=0;i<SPEC_FUNC_TABLE_SIZE;i++){
+		my_spec_func_table[i].addr = kallsyms_lookup_name(my_spec_func_table[i].name);
+		code_modify( &(my_spec_func_table[i]), (unsigned long)my_pre_handler_2);
+	}
 		
 	printk(KERN_INFO "write init\n");
     return 0;
@@ -454,7 +500,9 @@ static void __exit my_write_exit(void)
 	for(i=0;i<FUNC_TABLE_SIZE;i++){
 		code_restore( &(my_func_table[i]));
 	}
-	
+	for(i=0;i<SPEC_FUNC_TABLE_SIZE;i++){
+		code_restore( &(my_spec_func_table[i]));
+	}
 	//free the slab
 	for(i=0;i<CPU_NUM;i++){
 		for(j=0;j<ADDR_HEAD_SIZE;j++){
